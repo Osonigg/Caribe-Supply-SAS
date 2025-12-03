1b1346222fa2f8dab031c26189bdac420b24b527
Nabila C. Castillo <131215045+nabxo@users.noreply.github.com>
2025-12-02 18:57:03 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={Logo} className="App-logo" alt="Logo.jpeg" />
           <Navigation />
         </header>
   
